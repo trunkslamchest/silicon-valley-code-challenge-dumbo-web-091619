fdd208db8fbe62c99ae9955e50ce98053ce05c89
Austin Smith <trunk9slamchest@gmail.com>
2019-09-20 20:28:12 +0000

    Automatically backed up by Learn

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,21 @@
 class FundingRound
 
+	# attr_accessor
+	attr_reader :startup, :venture_capitalist, :type, :investment
+	# attr_writer
+
+	@@all = []
+
+	def initialize(startup, venture_capitalist, type, investment)
+		@startup = startup
+		@venture_capitalist = venture_capitalist
+		@type = type
+		@investment = investment
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
 end
